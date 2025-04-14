5950400505e2c5dc2dee236423135eba45ddc956
SergeyVl <sghhggvbg@gmail.com>
2025-04-14 13:03:14 +0300

    Create .description.php

---
+++ .description.php
@@ -0,0 +1,15 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = array(
+    "NAME" => "Универсальный калькулятор",
+    "DESCRIPTION" => "Гибкий калькулятор для различных расчетов",
+    "ICON" => "/images/icon.gif",
+    "PATH" => array(
+        "ID" => "utility",
+        "NAME" => "Утилиты"
+    ),
+    "CACHE_PATH" => "Y",
+    "COMPLEX" => "N"
+);
+?>
